99916fabf1d5c7e17d144c9d1f10b25fd45e1e1f
Abo Ellil <mr.abuellil@gmail.com>
2023-10-09 21:22:32 +0300

    10

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
 
+  images: {
+    domains: ["fakestoreapi.com","i.ibb.co"],
+  }
+
+}
 module.exports = nextConfig
+
